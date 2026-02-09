6de8d4bfa3cd4cb90a4db374a4400238fbf23de0
asfarmdima <dimkaolekseenko@gmail.com>
2026-02-09 21:26:16 +0200

    add global pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3002;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(PORT);
   console.log(`Server working in PORT - ${PORT}`);
 }
