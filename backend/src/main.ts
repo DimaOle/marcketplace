c85c5647eedc9ee4ea2aec3e0626ae13932982a8
asfarmdima <dimkaolekseenko@gmail.com>
2026-02-09 20:20:28 +0200

    add global prefix api

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3002;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   await app.listen(PORT);
   console.log(`Server working in PORT - ${PORT}`);
 }
