ee53732331fbc567cbaec8707192212dd63df714
asfarmdima <dimkaolekseenko@gmail.com>
2026-02-19 21:59:03 +0200

    add jwt token & auth guard

---
+++ backend/src/auth/config/index.ts
@@ -0,0 +1,1 @@
+export * from './async-option-jwt-module';
---
+++ backend/src/common/guards/auth.guard.ts
@@ -0,0 +1,34 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private jwt: JwtService) {}
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new ForbiddenException('no acces token');
+    }
+
+    try {
+      const payload = await this.jwt.verifyAsync(token);
+      request['user'] = payload;
+
+      return true;
+    } catch (e) {
+      throw new ForbiddenException('invalid token');
+    }
+  }
+
+  private extractTokenFromHeader(request: Request) {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
