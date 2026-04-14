23acf983ff1d9ce5718761e8899b4a4ca7efc1d4
Dima Olekseenko <dimkaolekseenko@gmail.com>
2026-04-14 23:59:43 +0300

    add co & s getCart & addCart

--- backend/src/cart/cart.controller.ts
+++ backend/src/cart/cart.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { CartService } from './cart.service';
+import { DataFromUser } from 'src/common/decorators';
+import { AuthGuard } from 'src/common/guards';
+import { AddCartDTO } from './dto';
 
+@UseGuards(AuthGuard)
 @Controller('cart')
 export class CartController {
   constructor(private readonly cartService: CartService) {}
+
+  @Get()
+  getCart(@DataFromUser('userId') userId: string) {
+    return this.cartService.getCart(userId);
+  }
+
+  @Post()
+  addCart(@DataFromUser('userId') userId: string, @Body() dto: AddCartDTO) {
+    return this.cartService.addToCart(userId, dto);
+  }
 }
--- backend/src/cart/cart.service.ts
+++ backend/src/cart/cart.service.ts
@@ -1,4 +1,59 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AddCartDTO } from './dto';
+import { Prisma } from 'src/generated/prisma/client';
 
 @Injectable()
-export class CartService {}
+export class CartService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getCart(userId: string) {
+    const cart = await this.prisma.cart.findUnique({
+      where: { userId },
+      include: { items: { include: { product: true } } },
+    });
+
+    if (!cart) return { totalPrice: 0 };
+
+    const totalPrice = cart.items.reduce((acc, item) => {
+      const allPrice = item.product.price.mul(item.quantity);
+      return acc.plus(allPrice);
+    }, new Prisma.Decimal(0));
+
+    return { ...cart, totalPrice };
+  }
+
+  async addToCart(userId: string, dto: AddCartDTO) {
+    const { productId, quantity } = dto;
+
+    let cart = await this.prisma.cart.findUnique({ where: { userId } });
+
+    if (!cart) {
+      cart = await this.prisma.cart.create({ data: { userId } });
+    }
+
+    const existingItem = await this.prisma.cartItem.findFirst({
+      where: { productId, cartId: cart.id },
+    });
+
+    if (existingItem) {
+      return this.prisma.cartItem.update({
+        where: { id: existingItem.id },
+        data: { quantity: existingItem.quantity + quantity },
+      });
+    }
+
+    const product = await this.prisma.product.findUnique({
+      where: { id: productId },
+    });
+
+    return this.prisma.cartItem.create({
+      data: {
+        cartId: cart.id,
+        productId: productId,
+        quantity: quantity,
+        price: product.price,
+      },
+    });
+  }
+}
