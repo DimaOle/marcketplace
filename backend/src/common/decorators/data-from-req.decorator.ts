8ae749c7e9646f006e5d27c4e7c78f55ff25148f
Dima Olekseenko <dimkaolekseenko@gmail.com>
2026-04-14 23:57:58 +0300

    delete lgo

--- backend/src/common/decorators/data-from-req.decorator.ts
+++ backend/src/common/decorators/data-from-req.decorator.ts
@@ -5,7 +5,6 @@ export type DataReqType = 'userId' | 'email' | 'role';
 export const DataFromUser = createParamDecorator(
   (data: DataReqType | undefined, ctx: ExecutionContext) => {
     const req = ctx.switchToHttp().getRequest();
-    console.log(req.user);
     if (!req.user) {
       return {};
     }
